aee535dcf0ef24a6cf439a3a8b0982f050b43aec
silpaprasad <silpasree9539@gmail.com>
2026-05-09 00:00:49 +0200

    Changes are added mistakennely for some  files that should be in 04 04 b branch.

--- room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/web/controller/RoomController.java
+++ room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/web/controller/RoomController.java
@@ -1,4 +1,32 @@
 package com.frankmoley.lil.roomwebapp.web.controller;
 
+
+import com.frankmoley.lil.roomwebapp.data.entity.RoomEntity;
+import com.frankmoley.lil.roomwebapp.data.repository.RoomRepository;
+import com.frankmoley.lil.roomwebapp.data.web.model.Room;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/rooms")
 public class RoomController {
+
+    private final RoomRepository roomRepository;
+    public RoomController(RoomRepository roomRepository){
+        this.roomRepository=roomRepository;
+    }
+
+    @GetMapping
+    public String getRoomsPage(Model model){
+        List<RoomEntity> roomEntities = this.roomRepository.findAll();
+        List<Room> rooms = new ArrayList<>(roomEntities.size());
+        roomEntities.forEach(e-> rooms.add(new Room(e.getRoomId(), e.getName(),e.getNumber(), e.getBedInfo())));
+        model.addAttribute("rooms", rooms);
+        return "rooms";
+    }
 }
